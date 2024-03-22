d730ce8772aeb81457d66740234f159f7f61e05c
KATERINA ADRIANA SENIOR <katyasenior.com>
2024-03-22 20:21:06 +0200

    flex box added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 console.log("hello world");
 
+let bg_color = "white";
+console.log(bg_color);
+
 button.addEventListener("click", function (event) {
   document.getElementById("light_or_dark");
+  if bg_color = "white" {document.body.style.background-color = black}
 });
