222d464c731f6a5e37beb52334d996a4c10f4484
Katya <katyasenior@gmail.com>
2024-03-24 20:01:13 +0000

    music added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # TechEdProjects
+
+Requirements acheived:
+
+Added sections including a header and footer to give the webpage structure.
+
+Added background images via CSS.
+
+Gave both the title class and the dark mode button absolute positioning.
+
+I first tried to replicate the website through my own observation but then used developer tools and inspect when requiring assistance.
+
+I also relied on the website W3Schools.
+
+I wanted to use javascript to change the images to hidden so as to leave a white background as the site's light mode, but I wasn't sure how to target the visibility within a class. I will work on this for future.
+
+I have acheived the stretch goal of adding audio.
--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 console.log("hello world");
 
-let bg_color = "white";
+const audio = document.querySelector("audio");
+
+audio.addEventListener("play", function (event) {
+  console.log("Playing", event);
+});
+
+let bg_color = "space";
 console.log(bg_color);
 
-button.addEventListener("click", function (event) {
+document.addEventListener("click", function (event) {
   document.getElementById("light_or_dark");
-  if bg_color = "white" {document.body.style.background-color = black}
+  if ((bg_color = "space")) {
+    var elements = document.getElementById("images");
+    visibility = hidden;
+    let bg_color = "white";
+    console.log(bg_color);
+  }
+  if ((bg_color = "white")) {
+    document.body.style.backgroundColor = "white";
+    let bg_color = "white";
+    console.log(bg_color);
+  }
 });
